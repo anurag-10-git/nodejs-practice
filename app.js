57bdf9080bf36d799665ec999ac986040a503c70
anurag-10-git <anuragmehra2011@gmail.com>
2022-12-01 17:43:29 +0530

    server code change

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const http = require('http');
 
+const routes = require('./routes');
 
 
-const server = http.createServer(function(req,res){
-    console.log(req);
-});
+const server = http.createServer(routes);
+
+// let buf1 = Buffer.from('hello');
+// let buff2= Buffer.from('world');
+
+// let Data = [buf1,buff2];
+
+// console.log(Data);
+// console.log(Buffer.concat(Data));
+// console.log(Buffer.concat(Data).toString());
 
 server.listen(3000);
 
---
+++ routes.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+const requestHandler = (req, res)=> {
+     const url = req.url;
+     const method = req.method;
+
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head><title>Node js</title></head>')
+        res.write('<body><form action="/message" method="POST"><input name="message" type="text"/><button type="submit">Submit Form</button></form></body>')
+        res.write('</html>')
+        return res.end();
+        }
+    
+        //below statement redirect to localhost 
+        if(url === '/message' && method === 'POST'){
+            const body = [];
+    
+            //if data is received run this function
+            req.on('data',(chunk)=>{
+                 body.push(chunk);
+                 console.log(chunk);
+            });
+    
+    
+           //on data parsing completed is received run this function
+            return req.on('end',()=>{
+                const parsedBody = Buffer.concat(body).toString(); //Buffer is stop for data
+                console.log(parsedBody);
+                const message = parsedBody.split('=')[1];
+    
+                //
+                fs.writeFile('message.txt',message,(error)=>{
+                    res.statusCode = 302;
+                    res.setHeader('Location','/');
+                    return res.end();  
+                });
+            });
+    
+        }
+    
+        res.setHeader('content-type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>Node js</title></head>')
+        res.write('<body><h1>welcome to node js</h1></body>')
+        res.write('</html>')
+        res.end();
+}
+
+module.exports = requestHandler;
